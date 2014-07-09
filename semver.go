50c0e52521a7fc0cf14f1b95948bc8246c97266b
Justin LeFebvre <justin@cloudlock.com>
2014-07-09 17:35:02 -0400

    much simpler logic without requiring a branch condition

--- semver.go
+++ semver.go
@@ -360,10 +360,7 @@ func containsOnly(s string, set string) bool {
 }
 
 func hasLeadingZeroes(s string) bool {
-	if len(s) > 1 {
-		return strings.HasPrefix(s, "0")
-	}
-	return false
+	return len(s) > 1 && s[0] == '0'
 }
 
 // Creates a new valid build version
